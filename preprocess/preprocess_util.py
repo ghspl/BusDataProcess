cb22b4ef11f37003ee1efa6e74248c3dd739860e
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-01-16 16:14:46 +0800

    添加将unix时间戳(毫秒)转换到本地时间的功能函数.

--- preprocess/preprocess_util.py
+++ preprocess/preprocess_util.py
@@ -5,3 +5,16 @@
 数据预处理自定义工具函数
 """
 
+import pandas as pd
+import time
+
+def systime2bj(unix_time):
+    format = '%Y-%m-%d %H:%M:%S'
+    time_array = time.localtime(unix_time/1000)  # 转换为秒级来处理
+    dt = time.strftime(format, time_array)
+    return dt
+
+
+if __name__ == '__main__':
+    unixtime = 1474238421303
+    print systime2bj(unixtime)
