98df7771658513acb3be146a124e3f753bcf4f89
Zhang Zhe <zhangzhe.zhe@qq.com>
2017-01-16 16:14:10 +0800

    删除重复GPS数据的行,保存为csv格式到新的目录下.

--- preprocess/preprocess_main.py
+++ preprocess/preprocess_main.py
@@ -32,8 +32,35 @@ if __name__ == "__main__":
         for root, dirs, files in os.walk(work_path):
             for dbfile in files:
                 data_txt_path = os.path.join(root, dbfile)
-                logging.debug('Now processing: %s', data_txt_path)
+                logging.info('Now processing: %s', data_txt_path)
                 df = pd.read_table(data_txt_path, sep=':', names=df_head)
-                print df[['lon','lat']][0:50]
+                logging.debug('raw data:------------------------------- ')
+                logging.debug(df[['lon','lat']][0:50])
+                # TODO: 采用直接删除重复行法(仅保留第一行)有点武断.
+                # 可以考虑先得出是否重复行的bool数组,然后对数组分析,
+                # 如果连续较多重复行,则全部删去(不保留第一行),若较少则保留
+                df = df.drop_duplicates(['lon','lat'])
+                # 使用了drop_duplicates函数删除重复行,提出无效的车辆静止时的数据
+                # 可能存在问题:当车辆实际运行过程中,轨迹存在相同的GPS坐标时,第二个会被误删去,不过影响不大
+                logging.debug('drop duplicates:-----------')
+                logging.debug(df[['lon', 'lat']][0:50])
+                logging.debug('remained df length is: %d' % len(df))
+                logging.debug('--------------------------------------------------------------------------\n\n')
 
+                if len(df) < 50:  # 剩余有效数据太少,直接忽略
+                    break
+                # 保存df到文件,csv格式,通用性更好
+                head1, day = os.path.split(root)
+                head2, month = os.path.split(head1)
+                head3, vehicle = os.path.split(head2)
+                csv_path = os.path.join(my_config.new_data_path, vehicle)
+                if not os.path.exists(csv_path):
+                    os.mkdir(csv_path)  # 以车辆编号新建文件夹
+                csv_path = os.path.join(csv_path, month+day+'.csv')
+
+                df.to_csv(csv_path, header=False)  # 不储存说明头
+                # df.to_csv(csv_path)  # 储存说明头
+
+            # if files:
+            #     break  # 测试用,只针对第一个文件夹做测试
         break  # 测试用,只针对第一个文件夹做测试
